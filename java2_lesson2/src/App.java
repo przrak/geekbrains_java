9589e99625c2cf057b624c65a94fe9dc2900825c
Dmitry.Bokach <dmitry.bokach@quantumsoft.ru>
2020-12-20 09:00:57 +0000

    feature task2 complete

---
+++ java2_lesson2/src/App.java
@@ -0,0 +1,43 @@
+import exceptions.MyArraySizeException;
+import exceptions.MyDataArrayException;
+
+/**
+ * @author Dmitriy Bokach
+ */
+public class App {
+    public static void main(final String[] args) {
+        final App app = new App();
+        final String[][] array = new String[][]{
+                {"1", "2", "3", "4"},
+                {"5", "6", "a", "8"},
+                {"9", "10", "11", "12"},
+                {"13", "14", "15", "16"}
+        };
+        try {
+            System.out.println("Sum elements result - " + app.format(array));
+        } catch (MyArraySizeException | MyDataArrayException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private int format(final String[][] twoDimensionalArray) throws MyArraySizeException, MyDataArrayException {
+        if (twoDimensionalArray.length != 4) {
+            throw new MyArraySizeException("Array length must be 4x4");
+        }
+        int sum = 0;
+        for (int i = 0; i < twoDimensionalArray.length; i++) {
+            if (twoDimensionalArray[i].length != 4) {
+                throw new MyArraySizeException("Array length must be 4x4");
+            }
+            for (int j = 0; j < twoDimensionalArray[i].length; j++) {
+                try {
+                    sum += Integer.parseInt(twoDimensionalArray[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new MyDataArrayException("Exception message: " + e.getMessage() + " we get NumberFormatException" +
+                            " \nInvalid data index: i = " + i + " and j = " + j + " \nSum before exception: " + sum);
+                }
+            }
+        }
+        return sum;
+    }
+}
---
+++ java2_lesson2/src/exceptions/MyArraySizeException.java
@@ -0,0 +1,10 @@
+package exceptions;
+
+/**
+ * @author Dmitriy Bokach
+ */
+public class MyArraySizeException extends RuntimeException {
+    public MyArraySizeException(String message) {
+        super(message);
+    }
+}
